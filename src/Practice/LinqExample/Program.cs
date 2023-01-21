43404a1a1e092979dce18f053953365c2e470006
Shamim448 <shamim.448@gmail.com>
2023-01-21 08:51:46 +0600

    Apply linq on string

---
+++ src/Practice/LinqExample/Program.cs
@@ -0,0 +1,7 @@
+﻿
+List<int> myCollections = new List<int> {20,85,46,4,27,46,58,14,76};
+var values  = from i in myCollections where i > 20 select i;
+foreach (var value in values)
+{
+    Console.Write(value + " ");
+}
