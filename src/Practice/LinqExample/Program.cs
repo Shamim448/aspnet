be457875e9db806ee5229eb2d7d499a1075c1e06
Shamim448 <shamim.448@gmail.com>
2023-01-21 09:22:15 +0600

    Apply linq on string

--- src/Practice/LinqExample/Program.cs
+++ src/Practice/LinqExample/Program.cs
@@ -1,7 +1,9 @@
 
-List<int> myCollections = new List<int> {20,85,46,4,27,46,58,14,76};
-var values  = from i in myCollections where i > 20 select i;
+//List<int> myCollections = new List<int> {20,85,46,4,27,46,58,14,76};
+List<string> myCollections = new List<string> {"Shamim", "Anika", "Saba", "Fatema", "Moina"};
+var values = from i in myCollections where i.Contains('m') select i;
 foreach (var value in values)
 {
     Console.Write(value + " ");
 }
+
