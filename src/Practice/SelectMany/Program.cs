b0a6344135f4474efe8735c7f86f8b4d0c1bf139
Shamim448 <shamim.448@gmail.com>
2023-01-22 15:38:04 +0600

    selectmany

--- src/Practice/SelectMany/Program.cs
+++ src/Practice/SelectMany/Program.cs
@@ -2,7 +2,15 @@
 using SelectMany;
 //Print all subject
 var allSubject = Student.GetStudents().SelectMany(s => s.Subjects).Distinct().ToList();
+//var allSubject = Student.GetStudents().SelectMany(s => s.Subjects).ToList();//all subject accept duplicates
 foreach(var subject in allSubject)
 {
     Console.WriteLine(subject);
+}
+//Print all student with subject
+
+var studentWithSubject = Student.GetStudents().SelectMany(s => s.Subjects, (student, subject) => new { StudentName = student.Name, Subject = subject });
+foreach (var v in studentWithSubject)
+{
+    Console.WriteLine(v.StudentName + " - " + v.Subject);
 }
