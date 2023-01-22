8381d9910077e4adf90b8c6974a24b4fcb4365ab
Shamim448 <shamim.448@gmail.com>
2023-01-22 15:14:33 +0600

    SelectMany

---
+++ src/Practice/SelectMany/Program.cs
@@ -0,0 +1,8 @@
+﻿
+using SelectMany;
+//Print all subject
+var allSubject = Student.GetStudents().SelectMany(s => s.Subjects).Distinct().ToList();
+foreach(var subject in allSubject)
+{
+    Console.WriteLine(subject);
+}
---
+++ src/Practice/SelectMany/Student.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectMany
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public List<string> Subjects { get; set; }
+        public static List<Student> GetStudents()
+        {
+            List<Student> listStudents = new List<Student> { 
+                new Student
+                {
+                    Name = "Shamim",
+                    Gender = "Male",
+                    Subjects= new List<string> {"Asp.Net", "C#"}
+                },
+               new Student
+                {
+                    Name = "Tom",
+                    Gender = "Male",
+                    Subjects = new List<string> { "ASP.NET", "C#" }
+                },
+                new Student
+                {
+                    Name = "Mike",
+                    Gender = "Male",
+                    Subjects = new List<string> { "ADO.NET", "C#", "AJAX" }
+                },
+                new Student
+                {
+                    Name = "Pam",
+                    Gender = "Female",
+                    Subjects = new List<string> { "WCF", "SQL Server", "C#" }
+                },
+                new Student
+                {
+                    Name = "Mary",
+                    Gender = "Female",
+                    Subjects = new List<string> { "WPF", "LINQ", "ASP.NET" }
+                },
+            };
+            return listStudents;
+        }
+    }
+}
