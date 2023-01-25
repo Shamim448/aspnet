e5f8c43a269aad2df337c36c54454717ae171328
Shamim448 <shamim.448@gmail.com>
2023-01-25 20:27:33 +0600

    Generics method and generics class

---
+++ src/Practice/GenericsMethod/GenericClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsMethod
+{
+    public class GenericClass
+    {
+        public int Add<T>(T a, T b)
+        {               
+            dynamic v1 = a;
+            dynamic v2 = b;
+            return v1 + v2;
+        }
+        public int Sub<T>(T a, T b)
+        {
+            dynamic v1 = a;
+            dynamic v2 = b;
+            return v1 - v2;
+        }
+        public int Mul<T>(T a, T b)
+        {
+            dynamic v1 = a;
+            dynamic v2 = b;
+            return v1 * v2;
+        }
+        public int div<T>(T a, T b)
+        {
+            dynamic v1 = a;
+            dynamic v2 = b;
+            return v1 - v2;
+        }
+    }
+}
---
+++ src/Practice/GenericsMethod/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using GenericsMethod;
+
+GMethod generics = new GMethod();
+{
+    bool result = generics.commpire<float>(4.5f, 4.5f);
+    Console.WriteLine(result);
+}
