973ae3ccb4c995c3f04895db3862faec7de2da40
Shamim448 <shamim.448@gmail.com>
2023-01-26 12:20:47 +0600

    List

---
+++ src/Practice/GenericsCollection/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+List<string> li = new List<string>();
+li.Add("Shamim");
+li.Add("Saba");
+li.Add("Fatema");
+li.Add("Apple");
+for(int i = 0; i < li.Count; i++)
+{
+    Console.WriteLine(li[i]);
+}
+//Capacity of link
+int t = li.Capacity;
+Console.WriteLine("Index Size : " + t);
+//Add value into the middle
+li.Insert(3, "Israt");
+foreach(string s in li)
+{
+    Console.WriteLine(s);
+}
+//Capacity of link after add another value
+int t1 = li.Capacity;
+Console.WriteLine("Number of Value : " + li.Count);
+Console.WriteLine("Index Size : " + t1);
+//Remove item using value and index
+li.Remove("Fatema");
+li.RemoveAt(2);
+foreach(string s in li)
+{
+    Console.WriteLine(s);
+}
