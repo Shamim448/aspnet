f466797073b5e9124ee5e32dfd05224af6504450
Shamim448 <shamim.448@gmail.com>
2023-01-31 22:46:07 +0600

    query syntex

--- src/Practice/SelectMany/Program.cs
+++ src/Practice/SelectMany/Program.cs
@@ -1,6 +1,23 @@
 ﻿
 using SelectMany;
 //Print all subject
+var result = from student in Student.GetStudents() from subject  in student.Subjects 
+             select subject.Distinct() ;
+foreach (var item in result)
+{
+    Console.WriteLine(item);
+}
+var s = from student in Student.GetStudents() select student.Name;
+foreach (var item in s)
+{
+    Console.WriteLine(item);
+}
+
+string[] a = { "shamim", "Saba", "Sopna"};
+var c = from d in a select d;
+foreach (var item in c) { Console.WriteLine(item); }
+
+
 var allSubject = Student.GetStudents().SelectMany(s => s.Subjects).Distinct().ToList();
 //var allSubject = Student.GetStudents().SelectMany(s => s.Subjects).ToList();//all subject accept duplicates
 foreach(var subject in allSubject)
