1328ac64549c0d4caf77ee202a36a8a3509634cd
Shamim448 <shamim.448@gmail.com>
2023-02-01 22:24:07 +0600

    Group By

---
+++ src/Practice/GroupBy/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using GroupBy;
+
+var result = from employee in Employee.GetAllEmployees() group employee by employee.Department;
+foreach (var item in result)
+{
+    Console.WriteLine("{0}, {1}",item.Key, item.Count()) ;
+}
