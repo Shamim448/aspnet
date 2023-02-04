9fdad8efb1b4d85a9b81337e465b8b8e37a40d4f
Shamim448 <shamim.448@gmail.com>
2023-02-04 08:50:03 +0600

    Linq-101-Sample

---
+++ src/Practice/Linq-101-Sample/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Linq_101_Sample;
+
+Console.WriteLine("Hello, World!");
+new Ordering().OrderbySyntax();
+new Ordering().OrderbyProperty();
+//new Ordering().OrderByProducts();
+new Ordering().ThenBySyntax();
+
+
