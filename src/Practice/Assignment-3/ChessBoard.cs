b54ff3c228c18dcf2115d79baa7fa4952912fbc2
Shamim448 <shamim.448@gmail.com>
2023-02-26 17:36:33 +0600

    2d board design and print ready

---
+++ src/Practice/Assignment-3/ChessBoard.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_3
+{
+    public class ChessBoard
+    {
+        public string[,] _chessBoard;
+        public const int DIMENTION = 8;
+        public string ChessBoardHorizontalSymbol { get; set; }
+        public string ChessBoardVarticalSymbol { get; set; }
+        public ChessBoard()
+        {
+            _chessBoard = new string[DIMENTION, DIMENTION];
+            ChessBoardHorizontalSymbol = "+---";
+            ChessBoardVarticalSymbol = "| ";
+        }
+        public void PrintChessBoard()
+        {
+            Console.WriteLine("    0   1   2   3   4   5   6   7");
+            for(int r = 0; r < DIMENTION; r++)
+            {
+                Console.Write("  ");
+                for(int c = 0; c < DIMENTION; c++)
+                {
+                    Console.Write(ChessBoardHorizontalSymbol);
+                }
+                Console.Write("+\n");//Print last + and new line
+                Console.Write(r + " ");
+                for (int c = 0; c <= DIMENTION; c++)
+                {
+                    Console.Write(ChessBoardVarticalSymbol + "  ");
+                }
+                Console.WriteLine();
+
+            }
+            Console.Write("  ");
+            for (int r = 0;r < DIMENTION; r++)
+            {
+                Console.Write(ChessBoardHorizontalSymbol);
+            }
+            Console.Write("+\n");
+
+        }
+      
+    }
+}
---
+++ src/Practice/Assignment-3/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Assignment_3;
+
+Console.WriteLine("Chess Board 2D Design");
+ChessBoard chessBoard = new ChessBoard();
+chessBoard.PrintChessBoard();
+
+
