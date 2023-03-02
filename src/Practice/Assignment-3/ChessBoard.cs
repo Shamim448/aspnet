891f367573e8260579d10b1c1fa2fafe8b4c148e
Shamim448 <shamim.448@gmail.com>
2023-03-02 15:55:55 +0600

    design

--- src/Practice/Assignment-3/ChessBoard.cs
+++ src/Practice/Assignment-3/ChessBoard.cs
@@ -18,36 +18,6 @@ namespace Assignment_3
             display.DissplatChessBoard(chessBoard);
         }
         
-        //public void PrintChessBoard()
-        //{
-        //    #region Print_Chess_Board
-        //    Console.WriteLine("    0   1   2   3   4   5   6   7");
-        //    for(int r = 0; r < DIMENTION; r++)
-        //    {
-        //        Console.Write("  ");
-        //        for(int c = 0; c < DIMENTION; c++)
-        //        {
-        //            Console.Write(ChessBoardHorizontalSymbol);
-        //        }
-        //        Console.Write("+\n");//Print last + and new line
-        //       Console.Write(r + " ");
-        //        for (int c = 0; c <= DIMENTION; c++)
-        //        {
-        //            Console.Write(ChessBoardVarticalSymbol + "  ");
-        //        }
-        //        Console.WriteLine();
-
-        //    }
-        //    Console.Write("  ");
-        //    for (int r = 0;r < DIMENTION; r++)
-        //    {
-        //        Console.Write(ChessBoardHorizontalSymbol);
-               
-        //    }
-        //    Console.Write("+\n");
-        //    Console.ReadLine();
-        //    #endregion
-        //}
-
+        
     }
 }
