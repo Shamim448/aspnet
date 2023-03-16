0218aab0a9f6e269296525834d49af8830de40b3
Shamim448 <shamim.448@gmail.com>
2023-03-16 17:56:13 +0600

    add chessgame

---
+++ src/ChessBoard/ChessBoardConsoleGame/Piece.cs
@@ -0,0 +1,30 @@
+﻿namespace ChessBoardConsoleGame
+{
+  public abstract class Piece {
+        public string Name { get; protected set; }
+        public bool IsWhite { get; protected set; }
+
+        public Piece(string name, bool isWhite)
+        {
+            Name = name;
+            IsWhite = isWhite;
+        }
+        public abstract bool IsValidMove(Square currentSquare, Square targetSquare, Board board);
+        public string GetSymbol(string type)
+        {
+                string chessType = type switch
+                {
+                    "Rook" => " ♜ ",
+                    "Knight" => " ♞ ",
+                    "Bishop" => " ♝ ",
+                    "Queen" => " ♛ ",
+                    "King" => " ♔ ", 
+                    "Pawn" => " ♙ ",
+                    _ => " ",
+                };
+                return chessType;
+        }
+   
+    }
+
+}
