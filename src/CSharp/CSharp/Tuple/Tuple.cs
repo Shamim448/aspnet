6320dac5bedab52065143dbe64866bea8b3506fe
Shamim448 <shamim.448@gmail.com>
2023-03-28 15:43:27 +0600

    Tuple Practice

---
+++ src/CSharp/CSharp/Tuple/Tuple.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tuple
+{
+    public class Tuples
+    {
+        public (int, int ) Calculation(int x, int y)
+        {
+            int sum = x + y;
+            int multiplication = x * y;
+
+            return (sum, multiplication);
+        }
+    }
+}
