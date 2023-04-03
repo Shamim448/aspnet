253b993c5211a6b093d9f9daf7649bc53b4a15b7
Shamim448 <shamim.448@gmail.com>
2023-04-03 13:09:55 +0600

    Comment add

--- src/CSharp/CSharp/MyORM/MyORM.cs
+++ src/CSharp/CSharp/MyORM/MyORM.cs
@@ -11,12 +11,10 @@ public class MyORM<G, T> where T : IIdBase<G>
     {
         _connectionString = connectionString;
     }
-   
     //Insert value
-    public int Insert(T entity)
+    public void Insert(T entity)
     {
         var tablename = typeof(T).Name;
-
         //StringBuilder columnName = new StringBuilder();
         PropertyInfo[] properties = typeof(T).GetProperties().Where(p => p.Name != "Id").ToArray();
        
@@ -24,21 +22,23 @@ public class MyORM<G, T> where T : IIdBase<G>
         string columnName = string.Join(", ", properties.Select(c => c.Name));
         //column name set as a parameter name
         string parametersName = string.Join(", ", properties.Select(p => $"@{p.Name}"));
-        
+        //sql query
         string sql = $"Insert into {tablename} ({columnName}) Values({parametersName})";
+        //call sqlconnection and open connection 
         using SqlConnection connection = new SqlConnection(_connectionString) ;
             if (connection.State != System.Data.ConnectionState.Open)
             {
                 connection.Open();
             }
+            //instialize sqlcommend it takes a sql query and a sqlconnection
             using SqlCommand cmd = new SqlCommand(sql, connection);
+         //initialize value in parameters
             foreach(var property in properties)
             {
                 cmd.Parameters.AddWithValue($"@{property.Name}", property.GetValue(entity));
-                
-                var a = property.GetValue(entity);
+                //check value get or not
+                var value = property.GetValue(entity);
             }
-            int effected = cmd.ExecuteNonQuery();
-          return effected;
+            cmd.ExecuteNonQuery();     
     }
 }
