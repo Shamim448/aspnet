87da22c0ab004da8ad4b6ba2769c26b773d9c10c
Shamim448 <shamim.448@gmail.com>
2023-04-04 11:06:33 +0600

    Apply Exception handling and implement GetById

--- src/CSharp/CSharp/MyORM/MyORM.cs
+++ src/CSharp/CSharp/MyORM/MyORM.cs
@@ -22,16 +22,13 @@ public class MyORM<G, T> where T : IIdBase<G>
         var tablename = entityInfo.GetObjectName();
         string columnName = entityInfo.GetColumn(); 
         string parametersName = entityInfo.GetParameters(); 
-        //sql query
+        //sql query for parametarized Insert
         string sql = $"Insert into {tablename} ({columnName}) Values({parametersName})";
         //call sqlconnection and open connection 
-        using SqlConnection connection = new SqlConnection(_connectionString);
-        if (connection.State != System.Data.ConnectionState.Open)
-        {
-            connection.Open();
-        }
+        SqlConnection connection = new SqlConnection(_connectionString);
+        
         //instialize sqlcommend it takes a sql query and a sqlconnection
-        using SqlCommand cmd = new SqlCommand(sql, connection);
+        SqlCommand cmd = new SqlCommand(sql, connection);
         //initialize value in parameters
         foreach (var property in entityInfo.GetProperties())
         {
@@ -39,46 +36,129 @@ public class MyORM<G, T> where T : IIdBase<G>
             //check value get or not
             var value = property.GetValue(entity);
         }
-        cmd.ExecuteNonQuery();
+        try
+        {
+            if (connection.State != System.Data.ConnectionState.Open)
+            {
+                connection.Open();
+                cmd.ExecuteNonQuery();
+                Console.WriteLine("Record Insert Successfully");
+            }
+        }
+        catch(SqlException ex)
+        {
+            Console.WriteLine("Error Generated. Details: " + ex.ToString());
+        }
+        finally { 
+            connection.Close(); 
+        }
+       
         #endregion
     }
     public void Update(T entity) { }
     public void Delete(G id) { }
-    public void GetById(G id)    { 
-        
+    public void GetById(G id)    {
+        //sql query for select all data
+        string sql = $"Select * from {type.Name} Where Id = {id}";
+        using var connection = new SqlConnection(_connectionString);  
+        using SqlCommand cmd = new SqlCommand(sql, connection);      
+        try
+        {
+            if (connection.State != System.Data.ConnectionState.Open)
+            {
+                connection.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                List<Dictionary<string, object>> rows = new List<Dictionary<string, object>>();
+                while (reader.Read())
+                {
+                    Dictionary<string, object> columns = new Dictionary<string, object>();
+                    foreach (var column in reader.GetColumnSchema())
+                    {
+                        columns.Add(column.ColumnName, reader[column.ColumnName]);
+                    }
+                    rows.Add(columns);
+                }
+                //Print Table Header
+                if(rows.Count > 0) {
+                    foreach (var col in rows[0].Keys)
+                    {
+                        Console.Write(col + "\t");
+                    }
+                    Console.WriteLine();
+                    //Print All Value
+                    foreach (var v in rows)
+                    {
+                        foreach (var k in v.Values)
+                        {
+                            Console.Write(k + "\t");
+                        }
+                        Console.WriteLine();
+                    }
+                } 
+                else { 
+                    Console.WriteLine($"There is no value in this {id} id."); 
+                }
+            }
+        }
+        catch (SqlException ex)
+        {
+            Console.WriteLine("Error Generated. Details: " + ex.ToString());
+        }
+        finally
+        {
+            connection.Close();
+        }
+
     }
     public void GetAll() {
-        //sql query
+        #region Get_All_Table_Data
+        //sql query for select all data
         string sql = $"Select * from {type.Name}";
-        using var connection = new SqlConnection(_connectionString);
-        if(connection.State != System.Data.ConnectionState.Open)
+        var connection = new SqlConnection(_connectionString);
+        SqlCommand cmd = new SqlCommand( sql, connection);
+        try
         {
-            connection.Open();
-        }
-        using SqlCommand cmd = new SqlCommand( sql, connection);
-        SqlDataReader reader = cmd.ExecuteReader();
-        List < Dictionary<string, object> > rows = new List < Dictionary<string, object> >();
-        while (reader.Read())
-        {
-            Dictionary<string, object> columns = new Dictionary<string, object>();
-           foreach(var column in reader.GetColumnSchema())
+            if (connection.State != System.Data.ConnectionState.Open)
             {
-                columns.Add(column.ColumnName, reader[column.ColumnName]);
+                connection.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                List<Dictionary<string, object>> rows = new List<Dictionary<string, object>>();
+                while (reader.Read())
+                {
+                    Dictionary<string, object> columns = new Dictionary<string, object>();
+                    foreach (var column in reader.GetColumnSchema())
+                    {
+                        columns.Add(column.ColumnName, reader[column.ColumnName]);
+                    }
+                    rows.Add(columns);
+                }
+                //Print Table Header
+                foreach (var col in rows[0].Keys)
+                {
+                    Console.Write(col + "\t");
+                }
+                Console.WriteLine();
+                //Print All Value
+                foreach (var v in rows)
+                {
+                    foreach (var k in v.Values)
+                    {
+                        Console.Write(k + "\t");
+                    }
+                    Console.WriteLine();
+                }
             }
-            rows.Add(columns);
         }
-        foreach (var col in rows[0].Keys)
+        catch (SqlException ex)
         {
-            Console.Write(col + "\t");
+            Console.WriteLine("Error Generated. Details: " + ex.ToString());
         }
-        Console.WriteLine();
-        foreach (var v in rows)
+        finally
         {
-            foreach (var k in v.Values)
-            {
-                Console.Write(k + "\t");
-            }
-            Console.WriteLine();
+            connection.Close();
         }
+       
+        
+        #endregion
     }
 }
--- src/CSharp/CSharp/MyORM/Program.cs
+++ src/CSharp/CSharp/MyORM/Program.cs
@@ -1,7 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 string DbConnection = "Server=.\\SQLEXPRESS;Database=Aspnetb8;User Id=aspnetb8;Password=123456;";
 Course course = new Course();
-course = new Course { Title = "Demo Course", Fees = 5000, IsActive = false, StartDate = new DateTime(2023, 04,25) };
+course = new Course { Title = "English", Fees = 5000, IsActive = false, StartDate = new DateTime(2023, 04,25) };
 var orm = new MyORM< int, Course>(DbConnection);
 //orm.Insert(course);
-orm.GetAll();
+//orm.GetAll();
+orm.GetById(15);
