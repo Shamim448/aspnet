14ead78ecb762da4eaf11e862a08e8590e1e7ece
Shamim448 <shamim.448@gmail.com>
2023-05-03 20:03:31 +0600

    file remove

---
+++ src/Practice/Files/ApplicationModule.cs
@@ -0,0 +1,26 @@
+﻿using Autofac;
+using FirstDemo.Application;
+using FirstDemo.Application.Features.Training.Repositories;
+using FirstDemo.Application.Services;
+using FirstDemo.Domain.Services;
+
+namespace FirstDemo.Application
+{
+    public class ApplicationModule : Module
+    {
+		private readonly string _connectionString;
+		private readonly string _migrationAssemblyName;
+
+		public ApplicationModule(string connectionString, string migrationAssemblyName)
+		{
+			_connectionString = connectionString;
+			_migrationAssemblyName = migrationAssemblyName;
+		}
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<CourseService>().As<ICourseService>()
+                .InstancePerLifetimeScope();
+		}
+    }
+}
