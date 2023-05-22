fc034288b2494fc13bf01d6b428a96f79c4d5088
Shamim448 <shamim.448@gmail.com>
2023-05-22 08:11:22 +0600

    Application Module

---
+++ Practice/GroupStudy/Crud.Application/ApplicationModule.cs
@@ -0,0 +1,28 @@
+﻿using Crud.Application.Services;
+using Crud.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crud.Application
+{
+    public class ApplicationModule : Module
+    {
+        private readonly string _connectionString;
+        private readonly string _migrationAssembly;
+        public ApplicationModule(string connectionString, string migrationsAssembly)
+        {
+            _connectionString = connectionString;
+            _migrationAssembly = migrationsAssembly;
+        }
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<UserService>().As<IUserService>();
+
+        }
+
+        
+    }
+}
