b0d4af7c9a8b1e12fd36cfc75ab33523262ddeae
Shamim448 <shamim.448@gmail.com>
2023-05-22 11:53:52 +0600

    Autofac binding done

---
+++ Practice/GroupStudy/Crud.web/WebModule.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+using Crud.web.Models;
+
+namespace Crud.web
+{
+    public class WebModule:Module
+    {
+        private readonly string _connectionString;
+        private readonly string _migrationsAssembly;
+        public WebModule(string connectionString, string migrationsAssembly)
+        {
+            _connectionString = connectionString;
+            _migrationsAssembly = migrationsAssembly;
+        }
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<UserListModel>().AsSelf().InstancePerLifetimeScope();
+        }
+    }
+}
