cdc7cb8bfa937341d65cf12dded9b7700d0110ca
Shamim448 <shamim.448@gmail.com>
2023-05-26 16:28:40 +0600

    Extension Method

---
+++ Practice/ExtentionMethod/EntentionMethodExample.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtentionMethod
+{
+    public static class EntentionMethodExample
+    {
+        public static string AddSmaileFace(this string smaileFace)
+        {
+            return smaileFace + "😊";
+        }
+    }
+}
---
+++ Practice/ExtentionMethod/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ExtentionMethod;
+
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+string message = " Hello, How are you? ";
+string result = message.AddSmaileFace();
+
+Console.WriteLine(message.AddSmaileFace());
+Console.WriteLine(result);
+
