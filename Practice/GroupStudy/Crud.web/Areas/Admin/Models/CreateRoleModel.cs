081e0d95dfc0a77947e33ce874803db1a3acc7ea
Shamim448 <shamim.448@gmail.com>
2023-06-30 10:43:44 +0600

    CreateRoleModel

--- Practice/GroupStudy/Crud.web/Areas/Admin/Models/CreateRoleModel.cs
+++ Practice/GroupStudy/Crud.web/Areas/Admin/Models/CreateRoleModel.cs
@@ -1,6 +1,36 @@
-﻿namespace Crud.web.Areas.Admin.Models
+﻿using Autofac;
+using Crud.Persistance.Features.Membership;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Crud.web.Areas.Admin.Models
 {
     public class CreateRoleModel
     {
+        [Required]
+        public string UserName { get; set; }
+
+        private RoleManager<ApplicationUserRole> _roleManager;
+        private UserManager<ApplicationUser> _userManager;
+        
+        public CreateRoleModel() { 
+
+        }
+        public CreateRoleModel(RoleManager<ApplicationUserRole> roleManager,
+            UserManager<ApplicationUser> userManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        internal void ResolveDependency (ILifetimeScope scope)
+        {
+            _roleManager = scope.Resolve<RoleManager<ApplicationUserRole>>();
+            _userManager = scope.Resolve<UserManager<ApplicationUser>>();
+        }
+
+
+
+
     }
 }
