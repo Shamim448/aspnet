0c0114932093ccdb8226251c680183640d44d965
Shamim448 <shamim.448@gmail.com>
2023-07-09 18:37:02 +0600

    userController & UserModel

---
+++ Practice/GroupStudy/Crud.API/Controllers/UsersController.cs
@@ -0,0 +1,76 @@
+﻿using Autofac;
+using Crud.API.Models;
+using Crud.Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Crud.API.Controllers
+{
+    [Route("v3/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly ILifetimeScope _scope;
+        private readonly ILogger<UsersController> _logger;
+
+        public UsersController(ILifetimeScope scope, ILogger<UsersController> logger)
+        {
+            _scope = scope;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<User> Get() { 
+            try
+            {
+                var model = _scope.Resolve<UserModel>();
+                return model.GetUsers();
+            }
+            catch(Exception ex) {
+                _logger.LogError(ex, "Couldn't get courses");
+                return null;
+            }
+        }
+        [HttpGet("{id}")]
+        public User Get(int id) {
+            var model = _scope.Resolve<UserModel>();
+            return model.GetUsers(id);
+        }
+        [HttpGet("{name}")]
+        public User Get(string name)
+        {
+            var model = _scope.Resolve<UserModel>();
+            return model.GetUsers(name);
+        }
+
+        [HttpPost()]
+        public IActionResult Post(UserModel model)
+        {
+            try {
+                model.ResolveDepenency(_scope);
+                model.CreateUser();
+                return Ok();
+            }
+            catch (Exception ex) {
+                _logger.LogError(ex, "Couldn't Updete user");
+                return BadRequest();
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                var model = _scope.Resolve<UserModel>();
+                model.DeleteUser(id);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Couldn't delete user");
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ Practice/GroupStudy/Crud.API/Models/UserModel.cs
@@ -0,0 +1,34 @@
+﻿using Autofac;
+using AutoMapper;
+using Crud.Application.Features.Training.Services;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace Crud.API.Models
+{
+    public class UserModel
+    {
+        private IUserService? _userService;
+        private IMapper _mapper;
+
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string? Address { get; set; }
+
+        public UserModel()
+        {
+
+        }
+        public UserModel(IUserService userService, IMapper mapper)
+        {
+            _userService = userService;
+            _mapper = mapper;
+        }
+        public void ResolveDepenency(ILifetimeScope scope)
+        {
+            _userService = scope.Resolve<IUserService>();
+            _mapper = scope.Resolve<IMapper>();
+        }
+    }
+}
