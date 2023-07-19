9b1408fc6f33db192aebf636850b66fcfa721a12
Shamim448 <shamim.448@gmail.com>
2023-07-19 16:22:31 +0600

    MigrationRunner

---
+++ Practice/GroupStudy/Crud.MigrationRunner/Program.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Identity.Client;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
+using Autofac.Extensions.DependencyInjection;
+using Autofac;
+using Crud.Persistance;
+using Crud.Application;
+using Crud.Infrastructure;
+
+namespace Crud.MigrationRunner
+{
+    public class Program
+    {
+        private static string _connectionString;
+        private static string _migrationAssemblyName;
+        private static IConfiguration _configuration;
+
+        static void Main(string[] args)
+        {
+            _configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json", false)
+                .AddEnvironmentVariables()
+                .Build();
+            _connectionString = _configuration.GetConnectionString("DefaultConnection");
+            _migrationAssemblyName = typeof(Program).Assembly.FullName;
+
+            //Serilog configuration
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Warning)
+                .Enrich.FromLogContext()
+                .ReadFrom.Configuration(_configuration)
+                .CreateLogger();
+
+            try {
+                Log.Information("Application Starting up");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex) {
+                Log.Fatal(ex, "Application start-up failed");
+            }
+            finally { 
+                Log.CloseAndFlush();
+            }
+        }
+
+        //Dependency Injection
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+            .UseSerilog()
+            .ConfigureContainer<ContainerBuilder>(builder =>
+            {
+                builder.RegisterModule(new PersistanceModule(_connectionString, _migrationAssemblyName));
+                builder.RegisterModule(new ApplicationModule());
+                builder.RegisterModule(new InfrastructureModule());
+            });
+    }
+}
