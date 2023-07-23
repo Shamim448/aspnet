bb4b4c30303289379276e9d8c921cb8327827abb
Shamim448 <shamim.448@gmail.com>
2023-07-23 15:49:51 +0600

    Worker service project create

---
+++ Practice/GroupStudy/Crud.EmailWorker/Program.cs
@@ -0,0 +1,10 @@
+using Crud.EmailWorker;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
