270d0f25713969ee0f1e0c7b8e4b96fb7fd8aa1a
Shamim448 <shamim.448@gmail.com>
2023-07-30 10:38:48 +0600

    Mock configuration done

--- Practice/GroupStudy/Tests/Crud.Application.Tests/AccountServiceTests.cs
+++ Practice/GroupStudy/Tests/Crud.Application.Tests/AccountServiceTests.cs
@@ -1,17 +1,49 @@
+using Autofac.Extras.Moq;
+using Crud.Application.Features.Training.Repositories;
+using Crud.Application.Features.Training.Services;
+using Moq;
+
 namespace Crud.Application.Tests
 {
     public class AccountServiceTests
-    {
-        [SetUp]
-        public void Setup()
+    { 
+        private AutoMock _mock;
+        private Mock<IAccountRepository> _accountRepositoryMock;
+        private Mock<IEmailService> _emailServiceMock;
+        private AccountService _accountService;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
         {
+            _mock = AutoMock.GetLoose();
+        }
+        [SetUp]
+        public void SetUp()
+        {
+            _accountRepositoryMock = _mock.Mock<IAccountRepository>();
+            _emailServiceMock = _mock.Mock<IEmailService>();
+            _accountService  = _mock.Create<AccountService>();
         }
         [TearDown]
-        public void Teardown() { }
-        [Test]
-        public void Test1()
+        public void Teardown() 
         {
-            Assert.Pass();
+            _accountRepositoryMock.Reset();
+            _emailServiceMock.Reset();
+        }
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            _mock?.Dispose();
+        }
+        [Test]
+        public void CreateUser_LargeUsername_TruncateUsername()
+        {
+            const string username = @"mynameisshamimhosenmynameisshamimhosen
+                                    mynameisshamimhosenmynameisshamimhosen";
+            string expectedResult = username.Substring(0, 30);
+            const string password = "fgdhgfhgngdfjkgj";
+
+            _accountService.CreateAccount(username, password);
         }
     }
 }
