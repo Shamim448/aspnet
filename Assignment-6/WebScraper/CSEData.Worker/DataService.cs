fb99021131f869c191dbda37f5d1d0088ccb43f0
Shamim448 <shamim.448@gmail.com>
2023-08-01 16:36:34 +0600

    connect logger in DataService

--- Assignment-6/WebScraper/CSEData.Worker/DataService.cs
+++ Assignment-6/WebScraper/CSEData.Worker/DataService.cs
@@ -8,6 +8,11 @@ namespace CSEData.Worker
 {
     public class DataService : BackgroundService
     {
+        private readonly ILogger<DataService> _logger;
+        public DataService(ILogger<DataService> logger)
+        { 
+            _logger = logger;
+        }
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
