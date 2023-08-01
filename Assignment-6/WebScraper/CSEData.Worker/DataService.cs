77ad59d5a7b8f86ed34e5dc196426638d18e6d4d
Shamim448 <shamim.448@gmail.com>
2023-08-01 18:36:20 +0600

    all field select done

--- Assignment-6/WebScraper/CSEData.Worker/DataService.cs
+++ Assignment-6/WebScraper/CSEData.Worker/DataService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,13 +22,19 @@ namespace CSEData.Worker
                 try 
                 {
                     //Write my logic here
+                    GetLisByUrl("https://www.cse.com.bd/market/current_price");
+                    //foreach(var name in stockConeName)
+                    //{
+                    //    await Console.Out.WriteLineAsync(name);
+                    //    _logger.LogInformation($"{name}");
+                    //}
 
                 }
                 catch(Exception ex) 
                 { 
                     _logger.LogError(ex, ex.Message);
                 }
-                await Task.Delay(1000, stoppingToken);
+                await Task.Delay(90000, stoppingToken);
             }
         }
         public override Task StartAsync(CancellationToken cancellationToken)
@@ -39,6 +46,44 @@ namespace CSEData.Worker
         {
             _logger.LogInformation("Service Stoped");
             return base.StopAsync(cancellationToken); 
+        }
+        static HtmlDocument GetDocument(string url)
+        {
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web.Load(url);
+            return doc;
+        }
+        static void GetLisByUrl(string url){
+            List<string> stockCodeList = new List<string>();
+            List<string> Price = new List<string>();
+
+            HtmlDocument doc = GetDocument(url);
+            HtmlNodeCollection nodes = doc.DocumentNode.SelectNodes(xpath: "//td/a");
+            //HtmlNodeCollection nodes1 = doc.DocumentNode.SelectNodes(xpath: "//td[@class='trdPrice']/span");
+            HtmlNodeCollection SL = doc.DocumentNode.SelectNodes(xpath: "//tr/td[1]");
+            HtmlNodeCollection StockCode = doc.DocumentNode.SelectNodes(xpath: "//tr/td[2]");
+            HtmlNodeCollection LTP = doc.DocumentNode.SelectNodes(xpath: "//tr/td[3]");
+            HtmlNodeCollection Open = doc.DocumentNode.SelectNodes(xpath: "//tr/td[4]");
+            HtmlNodeCollection High = doc.DocumentNode.SelectNodes(xpath: "//tr/td[5]");
+            HtmlNodeCollection Low = doc.DocumentNode.SelectNodes(xpath: "//tr/td[6]");
+            HtmlNodeCollection Volumn = doc.DocumentNode.SelectNodes(xpath: "//tr/td[10]");
+            foreach (HtmlNode node in nodes)
+            {
+                stockCodeList.Add(node.InnerText);               
+            }
+            foreach (var node in stockCodeList)
+            {
+                Console.WriteLine(node);
+            }
+            foreach (HtmlNode node in LTP)
+            {
+                Price.Add(node.InnerText);
+            }
+            foreach (var node in Price)
+            {
+                Console.WriteLine(node);
+            }
+
         }
     }
 }
