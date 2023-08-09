a25f0a2016de8bf2a72bac4fb6d8a7859cf85aaf
Shamim448 <shamim.448@gmail.com>
2023-08-09 19:03:29 +0600

    Logger added

--- Assignment-6/WebScrapper/CSEData.Worker/WorkerModule.cs
+++ Assignment-6/WebScrapper/CSEData.Worker/WorkerModule.cs
@@ -5,17 +5,15 @@ using CSEData.Worker;
 internal class WorkerModule : Module
 {
 
-    private readonly IConfiguration _configuration;
-    public WorkerModule( IConfiguration configuration)
-    {     
-        _configuration = configuration;
-    }
+    //private readonly IConfiguration _configuration;
+    //public WorkerModule( IConfiguration configuration)
+    //{     
+    //    _configuration = configuration;
+    //}
     protected override void Load(ContainerBuilder builder)
     {
         base.Load(builder);
         builder.RegisterType<Worker>().AsSelf().InstancePerLifetimeScope();
-        //builder.RegisterType<CompanyCreateModel>().AsSelf().InstancePerLifetimeScope();
-        //builder.RegisterType<PriceCreateModel>().AsSelf().InstancePerLifetimeScope();     
-        //builder.RegisterType<WebScraperModel>().AsSelf().InstancePerLifetimeScope();     
+         
     }
 }
