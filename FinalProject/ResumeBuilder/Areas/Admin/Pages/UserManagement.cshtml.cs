c169470df5dc48d46b24b2397931ba5708971f69
Shamim448 <shamim.448@gmail.com>
2023-08-14 18:50:29 +0600

    Final project add Usermanagement page design

---
+++ FinalProject/ResumeBuilder/Areas/Admin/Pages/UserManagement.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ResumeBuilder.Areas.Admin.Pages
+{
+    public class UserManagementModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
