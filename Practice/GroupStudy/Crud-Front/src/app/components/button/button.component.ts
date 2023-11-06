c10b94b70b52e4e8c773fca1df16cd87186a34ca
Shamim448 <shamim.448@gmail.com>
2023-11-06 12:06:33 +0600

    field create

--- Practice/GroupStudy/Crud-Front/src/app/components/button/button.component.ts
+++ Practice/GroupStudy/Crud-Front/src/app/components/button/button.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.css']
 })
-export class ButtonComponent {
+export class ButtonComponent implements OnInit{
+@Input() text:string="";
+@Input() color: string="";
 
+constructor(){}
+ngOnInit(): void {
+  
+}
 }
